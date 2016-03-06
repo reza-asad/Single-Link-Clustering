6232f1d8b9dd83d48c8d5b4035d2051df632096d
reza-asad <rayza.asad@gmail.com>
2016-03-06 14:36:48 -0800

    first commit

---
+++ greedy_algorithm.py
@@ -0,0 +1,9 @@
+# Reza Asad
+# Algorithm Class
+# March 6th, 2016
+######################### Algorithms ########################
+# A greedy algrithm that finds the max-spacing k clusters
+def single_link_clustering(G):
+    pass
+######################## Main ###############################
+# Pre process the data
